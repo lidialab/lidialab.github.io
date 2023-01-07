a7ec2e751b5d3b00e0c6f4f8a640a5d34b780cae
Lidia Pellizzaro <lidia.pellizzaro@hotmail.it>
2023-01-07 21:18:03 +0100

    upd

--- utyWP.js
+++ utyWP.js
@@ -1,4 +1,89 @@
 // ============================================================
+// funzioni di apertura
+// ============================================================
+
+function apriSottoProgettiCore() {
+   void( window.open( 'https://translate.wordpress.org/projects/wp/dev/it/default/') );
+   void( window.open( 'https://translate.wordpress.org/projects/wp/dev/admin/it/default/') );
+   void( window.open( 'https://translate.wordpress.org/projects/wp/dev/admin/network/it/default/') );
+   void( window.open( 'https://translate.wordpress.org/projects/wp/dev/cc/it/default/') );
+}
+
+function apriTemiTwenty() {
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-themes/twentytwentythree') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-themes/twentytwentytwo') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-themes/twentytwentyone') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-themes/twentytwenty') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-themes/twentynineteen') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-themes/twentyseventeen') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-themes/twentysixteen') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-themes/twentyfifteen') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-themes/twentyfourteen') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-themes/twentythirteen') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-themes/twentytwelve') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-themes/twentyeleven') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-themes/twentyten') );
+}
+
+function apriPluginPrincipali() {
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-plugins/gutenberg') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-plugins/classic-editor') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-plugins/health-check') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-plugins/wordpress-importer') );
+   //void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-plugins/buddypress') );
+   //void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-plugins/bbpress') );
+   //void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-plugins/akismet') );
+   //void( window.open( 'https://translate.wordpress.org/locale/it/default/wp-plugins/jetpack') );
+}
+
+function apriProgettiMeta() {
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/meta/wordpress-org/') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/meta/plugins-v3/') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/meta/themes/') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/meta/rosetta/') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/meta/p2-breathe/') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/meta/o2') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/meta/forums/') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/meta/get-involved/') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/meta/pattern-directory/') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/meta/wordcamp/') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/meta/openverse/') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/meta/browsehappy/') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/meta/learn-wordpress/') );
+}
+
+
+function apriProgettiPrincipali() {
+   // WP Core
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/wp/dev') );
+   apriSottoProgettiCore();
+
+   // Meta
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/apps/') );
+   apriProgettiMeta();
+
+   // APP
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/apps/') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/apps/ios/') );
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/apps/android/') );
+
+   // Plugin WP
+   apriPluginPrincipali();
+
+   // Temi WP
+   apriTemiTwenty();
+
+   // Pattern
+   void( window.open( 'https://translate.wordpress.org/locale/it/default/patterns/core/') );
+   void( window.open( 'https://translate.wordpress.org/projects/patterns/core/it/default/') );
+
+   // Temi WP vecchi
+   //void( window.open( 'https://wordpress.org/themes/classic/') );
+   //void( window.open( 'https://wordpress.org/themes/default') );
+}
+
+
+// ============================================================
 // variabili e costanti
 // ============================================================
 const myLocale = "it_IT";
@@ -14,6 +99,9 @@ nomiPluginWPorg = ["gutenberg","classic-editor","health-check","wordpress-import
 nomiPluginRilevanti = ["bbpress","buddypress"]
 
 nomiMeta = ["wordpress-org","plugins-v3","themes","rosetta","p2-breathe","o2","forums","get-involved","pattern-directory","wordcamp","openverse","browsehappy","learn-wordpress"];
+
+var elenco = "";
+var insiemeSpunte = "";
 
 // ============================================================
 // funzione
@@ -81,9 +169,7 @@ function componiTagLink(url, testo) {
 }
 
 // ============================================================
-// script
-//
-// esempio: https://translate.wordpress.org/locale/it/default/meta/plugins-v3
+// funzione
 // ============================================================
 function elencoProgetti(tipoProj, slugProgetti, loc) {
 
@@ -145,9 +231,7 @@ function elencoProgetti(tipoProj, slugProgetti, loc) {
 }
 
 // ============================================================
-// script
-//
-// esempio: https://translate.wordpress.org/locale/it/default/meta/plugins-v3
+// funzione
 // ============================================================
 function elencoProgettiCore() {
 
@@ -164,6 +248,26 @@ function elencoProgettiCore() {
     return stringa;
 }
 
+// ============================================================
+// funzione
+// ============================================================
+function creaFieldset(etichetta, nomi) {
+
+    var  stringa = "";
+
+    stringa = `<legend>${etichetta}</legend>`;
+    for ( var ix = 0 ; ix < nomi.length; ix++ ) 
+        { 
+            stringa = stringa + `<div><label><input type=checkbox name="${nomi[ix]}" value="${nomi[ix]}">${nomi[ix]}</label></div>`;
+    }
+
+    return stringa;
+}
+
+
+// ============================================================
+// script
+// ============================================================
 
 elenco = elencoProgettiCore();
 document.getElementById('sez-core').innerHTML = elenco; 
@@ -171,11 +275,20 @@ document.getElementById('sez-core').innerHTML = elenco;
 elenco = elencoProgetti("meta", nomiMeta, myLoc);
 document.getElementById('sez-meta').innerHTML = elenco; 
 
-elenco = elencoProgetti("wp-themes", nomiTemiWPorg, myLoc, slug);
+elenco = elencoProgetti("wp-themes", nomiTemiWPorg, myLoc);
 document.getElementById('sez-temi-WP').innerHTML = elenco; 
 
-elenco = elencoProgetti("wp-plugins", nomiPluginWPorg, myLoc, slug);
+elenco = elencoProgetti("wp-plugins", nomiPluginWPorg, myLoc);
 document.getElementById('sez-plugin-WP').innerHTML = elenco; 
 
-elenco = elencoProgetti("wp-plugins", nomiPluginRilevanti, myLoc, slug);
+elenco = elencoProgetti("wp-plugins", nomiPluginRilevanti, myLoc);
 document.getElementById('sez-plugin-rilevanti').innerHTML = elenco; 
+
+insiemeSpunte = creaFieldset("Plugin", nomiPluginWPorg);
+document.getElementById('fieldset-plugin-wp-org').innerHTML = insiemeSpunte; 
+
+insiemeSpunte = creaFieldset("Meta", nomiMeta);
+document.getElementById('fieldset-meta').innerHTML = insiemeSpunte; 
+
+insiemeSpunte = creaFieldset("Temi", nomiTemiWPorg);
+document.getElementById('fieldset-temi').innerHTML = insiemeSpunte; 
